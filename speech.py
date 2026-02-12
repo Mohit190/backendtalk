6a1ffd1643fd7e9719b5b226f32775af96936abb
Mohit190 <32761989+Mohit190@users.noreply.github.com>
2026-02-12 19:08:20 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -1,7 +1,8 @@
 import whisper
 
-model = whisper.load_model("base")
+model = whisper.load_model("tiny")
 
 def speech_to_text(path):
     result = model.transcribe(path)
     return result["text"]
+
